06f587d4e2ecbe7af87eed8166a87c9507f777b4
Joivre Charles <astemcode@gmail.com>
2021-01-03 19:03:47 -0500

    Employee Prompt
    The prompt for the employee sections were added

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const (connect) = require(`http`);
+const connect = require(`http`);
 const mysql = require(`mysql`);
-const questions = require(`inquirer`);
+const inquirer = require(`inquirer`);
+const { exit } = require("process");
 
 let connection = mysql.createConnection({
     host: `localhost`,
     port: 3306,
     user: `root`,
-    password: `password`,
+    password: `employees`,
     database: `employee_db`
 })
 
@@ -18,53 +19,110 @@ function init() {
 			{
 				type: `list`,
 				message: `Which task would you like to complete today?`,
-				choices: [ `Add Departments, Roles, or Employees`,'Add Employee', `Remove Employee`, `Add Role`, `Remove Role`, `Add Department`, `Remove Department`, `View Departments, Roles, or Employees`, `View All Employees`, 'View Employees By Department', `View All Employees by Manager`,  `Update Departments, Roles, or Employees`, `Update Employee Role`, `Update Employee Manager`, `Exit`],
+				choices: [ `Add Employees`, `Add Departments`, `Add Roles`, `View Departments`, `View Roles`, `View Employees`, `View Employees by Department`, `Update Employee Role`, `Exit`],
 				name: `Selection`
 			}
 		])
 		.then((response) => {
 			switch (response.Selection) {
-				case `Add Departments, Roles, or Employees`:
-					addInfo();
+                case `Add Employees`:
+					addEmployee();
+                    break;
+				case `Add Departments`:
+					addDepartment();
 					break;
-				case `View Departments, Roles, or Employees`:
-					viewInfo();
+				case `Add Roles`:
+					addRole();
 					break;
-				case `Update Employee roles`:
-					updateRoles();
-					break;
+                case `View Departments`:
+                        viewDepartments();
+                        break;
+                case `View Roles`:
+                    viewRoles();
+                    break;
+                case `View Employees`:
+                    viewEmployees();
+                    break;
+                case `View Employees by Department`:
+                    empDept();
+                    break;
+                case `Update Employee Role`:
+                    updateRole();
+                    break;
+                case `Exit`:
+                    exit()
+                    break;
 			}
 		});
     }
     
+function addEmployee(){
+
+    inquirer
+    .prompt([
+        {
+            type: `input`,
+            message: ` What is the first name of the employee you would like to add?`,
+            name: `firstName`,
+        },
+        {
+            type: `input`,
+            message: ` What is the last name of the employee you would like to add?`,
+            name: `lastName`,
+        },
+        {
+            type: `input`,
+            message: ` What is the Role ID of the new Employee you would like to add?`,
+            name: `roleID`,
+        },
+        {
+            type: `input`,
+            message: `What is the ID of the manager for the new Employee that you would like to add?`,
+            name: `managerID`,
+        }
+    ])
+        .then((response) => {
+          let query = connection.query("INSERT INTO employee SET ?", {
+            first_name: response.firstName,
+            last_name: response.lastName,
+            role_id: response.roleID,
+            manager_id: response.managerID
+          },
+          (err, res) => {
+            if(err) throw err;
+            console.log(`${res.affectedRows} was added to the Employee list!`);
+            init();
+          })
+        })
+}
 
 
     init();
         
 
-    const actions = [
+    // const actions = [
 
-        {
-            type: "list",
-            name: "actions",
-            message: "What would you like to to?",
-            choices: [
+    //     {
+    //         type: "list",
+    //         name: "actions",
+    //         message: "What would you like to to?",
+    //         choices: [
     
-                "Add new employee",
-                "View all employees",
-                "View employees by department",
-                "Update employee role",
-                "View all roles",
-                "Add role",
-                "View all departments",
-                "Add department",
-                "Exit"
+    //             "Add new employee",
+    //             "View all employees",
+    //             "View employees by department",
+    //             "Update employee role",
+    //             "View all roles",
+    //             "Add role",
+    //             "View all departments",
+    //             "Add department",
+    //             "Exit"
     
-            ]
+    //         ]
     
-        }
-    ]
+    //     }
+    // ]
     
     
     
-    module.exports = { actions }
+    // module.exports = { actions }
--- sql/schema.sql
+++ sql/schema.sql
@@ -6,7 +6,6 @@ USE employee_db;
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
-
 );
 
 CREATE TABLE role (
@@ -26,3 +25,4 @@ CREATE TABLE employee (
 FOREIGN KEY (manager_id) REFERENCES employee (id),
 FOREIGN KEY(role_id) REFERENCES role (id)
 );
+
